c4c058df2586f9ff0bb810abbc7c247cd687eb2f
Fernandoxxi <manriquefernando2104@gmail.com>
2025-09-26 07:53:21 -0500

    feat: arreglo de refresh de pagina

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // importante para producción
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    open: true, // opcional, abre el navegador al iniciar
+  },
+});
